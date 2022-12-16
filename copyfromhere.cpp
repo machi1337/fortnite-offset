b071c62c699d733a849cf6f18e48134e4447ce19
machi1337 <120744382+machi1337@users.noreply.github.com>
2022-12-16 20:08:50 +0200

    Create copyfromhere.cpp

---
+++ copyfromhere.cpp
@@ -0,0 +1,24 @@
+//OFFSET UPDATED 12/16/22 BY MACHI
+
+
+uworld : 0xC6B5D08
+GObject : 0xC501850
+BoneMatrix : 0X4E94C18
+ProjectWorldToScreen : 0x86E48A4
+Mesh : 0x2F0
+AActors : 0x98
+LocalPawn : 0x310
+playerController : 0x30
+OwningGameInstance: 0x1A8
+CurrentActorMesh: 0x2f0
+RelativeLocation: 0x129
+RelativeRotation: 0x140
+PlayerController: 0x30
+RootComponent: 0x188
+PlayerState: 0x290
+LocalPlayers: 0x38
+TeamIndex: 0x1110
+
+
+
+ImageBase:7FF665C90000
